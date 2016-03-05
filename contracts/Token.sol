9a3f315b328ba6e22c9d5dfcf259b67c4f63d72e
WenJinGe <wenjinge0424@outlook.com>
2016-03-05 21:25:44 +0900

    contract skeleton - WIP

---
+++ contracts/Token.sol
@@ -0,0 +1,45 @@
+import "./TokenInterface.sol";
+contract Token is TokenInterface {
+
+  modifier noEther() {if (msg.value > 0) throw; _}
+
+  function transfer(address _to, uint256 _value) noEther returns (bool success) {
+    if (balances[msg.sender] >= _value && _value > 0) {
+      balances[msg.sender] -= _value;
+      balances[_to] += _value;
+      Transfer(msg.sender, _to, _value);
+      success = true;
+    } else {
+      success = false;
+    }
+    return success;
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) noEther returns (bool success) {
+    if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
+      balances[_to] += _value;
+      balances[_from] -= _value;
+      allowed[_from][msg.sender] -= _value;
+      Transfer(_from, _to, _value);
+      success = true;
+    } else {
+      success = false;
+    }
+    return success;
+  }
+
+  function approve(address _spender, uint256 _value) returns (bool success) {
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  function balanceOf(address _owner) constant returns (uint256 balance) {
+    return balances[_owner];
+  }
+
+
+  function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+    return allowed[_owner][_spender];
+  }
+}
