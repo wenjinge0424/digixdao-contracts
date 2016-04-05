fd804883a9b978a0373830fbd535d91a229989c0
WenJinGe <wenjinge0424@outlook.com>
2016-04-05 12:11:35 -0700

    separate badge and tokens into individual EIP-20 contracts

--- contracts/Interfaces.sol
+++ contracts/Interfaces.sol
@@ -63,16 +63,8 @@ contract ConfigInterface {
 
 }
 
-contract TokenInterface {
+contract BadgeInterface {
 
-  struct User {
-    bool locked;
-    uint256 balance;
-    uint256 badges;
-    mapping (address => uint256) allowed;
-  }
-
-  mapping (address => User) users;
   mapping (address => uint256) balances;
   mapping (address => mapping (address => uint256)) allowed;
   mapping (address => bool) seller;
@@ -84,7 +76,6 @@ contract TokenInterface {
 
   /// @return total amount of tokens
   uint256 public totalSupply;
-  uint256 public totalBadges;
 
   /// @param _owner The address from which the balance will be retrieved
   /// @return The balance
@@ -100,11 +91,62 @@ contract TokenInterface {
   /// @return Whether the transfer was successful or not
   function transfer(address _to, uint256 _value) returns (bool success);
 
-  /// @notice send `_value` badges to `_to` from `msg.sender`
+  /// @notice send `_value` tokens to `_to` from `_from` on the condition it is approved by `_from`
+  /// @param _from The address of the sender
   /// @param _to The address of the recipient
   /// @param _value The amount of tokens to be transfered
   /// @return Whether the transfer was successful or not
-  function sendBadge(address _to, uint256 _value) returns (bool success);
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+
+  /// @notice `msg.sender` approves `_spender` to spend `_value` tokens on its behalf
+  /// @param _spender The address of the account able to transfer the tokens
+  /// @param _value The amount of tokens to be approved for transfer
+  /// @return Whether the approval was successful or not
+  function approve(address _spender, uint256 _value) returns (bool success);
+
+  /// @param _owner The address of the account owning tokens
+  /// @param _spender The address of the account able to transfer the tokens
+  /// @return Amount of remaining tokens of _owner that _spender is allowed to spend
+  function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+
+  /// @notice mint `_amount` of tokens to `_owner`
+  /// @param _owner The address of the account receiving the tokens
+  /// @param _amount The amount of tokens to mint
+  /// @return Whether or not minting was successful
+  function mint(address _owner, uint256 _amount) returns (bool success);
+
+  function registerDao(address _dao) returns (bool success);
+  function registerSeller(address _tokensales) returns (bool success);
+
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  event SendBadge(address indexed _from, address indexed _to, uint256 _amount);
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+contract TokenInterface {
+
+  mapping (address => uint256) balances;
+  mapping (address => mapping (address => uint256)) allowed;
+  mapping (address => bool) seller;
+
+  address config;
+  address owner;
+  address dao;
+  address public badgeLedger;
+  bool locked;
+
+  /// @return total amount of tokens
+  uint256 public totalSupply;
+
+  /// @param _owner The address from which the balance will be retrieved
+  /// @return The balance
+  function balanceOf(address _owner) constant returns (uint256 balance);
+
+  /// @notice send `_value` tokens to `_to` from `msg.sender`
+  /// @param _to The address of the recipient
+  /// @param _value The amount of tokens to be transfered
+  /// @return Whether the transfer was successful or not
+  function transfer(address _to, uint256 _value) returns (bool success);
 
   /// @notice send `_value` tokens to `_to` from `_from` on the condition it is approved by `_from`
   /// @param _from The address of the sender
@@ -137,11 +179,10 @@ contract TokenInterface {
   function mintBadge(address _owner, uint256 _amount) returns (bool success);
 
   function registerDao(address _dao) returns (bool success);
-
   function registerSeller(address _tokensales) returns (bool success);
 
   event Transfer(address indexed _from, address indexed _to, uint256 _value);
-  event SendBadge(address indexed _from, address indexed _to, uint256 _amount);
+  event Mint(address indexed _recipient, uint256 indexed _amount);
   event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
 
--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,5 +1,78 @@
 import "./Interfaces.sol";
 
+contract Badge  {
+  mapping (address => uint256) balances;
+  mapping (address => mapping (address => uint256)) allowed;
+  mapping (address => bool) seller;
+
+  address config;
+  address owner;
+  address dao;
+  bool locked;
+
+  /// @return total amount of tokens
+  uint256 public totalSupply;
+
+  modifier ifOwner() {
+    if (msg.sender != owner) throw;
+    _
+  }
+
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  event Mint(address indexed _recipient, uint256 indexed _amount);
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+  function Badge(address _config) {
+    config = _config;
+    owner = msg.sender;
+  }
+
+  function balanceOf(address _owner) constant returns (uint256 balance) {
+    return balances[_owner];
+  }
+
+  function transfer(address _to, uint256 _value) returns (bool success) {
+    if (balances[msg.sender] >= _value && _value > 0) {
+      balances[msg.sender] -= _value;
+      balances[_to] += _value;
+      Transfer(msg.sender, _to, _value);
+      success = true;
+    } else {
+      success = false;
+    }
+    return success;
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+    if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
+      balances[_to] += _value;
+      balances[_from] -= _value;
+      allowed[_from][msg.sender] -= _value;
+      Transfer(_from, _to, _value);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function approve(address _spender, uint256 _value) returns (bool success) {
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+    return allowed[_owner][_spender];
+  }
+
+  function mint(address _owner, uint256 _amount) ifOwner returns (bool success) {
+    totalSupply += _amount;
+    balances[_owner] += _amount;
+    return true;
+  }
+
+}
+
 contract Token is TokenInterface {
 
   modifier noEther() {
@@ -27,21 +100,18 @@ contract Token is TokenInterface {
     owner = msg.sender;
     address _initseller = ConfigInterface(_config).getConfigAddress("sale1:address");
     seller[_initseller] = true; 
+    badgeLedger = new Badge(_config);
     locked = false;
   }
 
   function balanceOf(address _owner) constant returns (uint256 balance) {
-    return users[_owner].balance;
-  }
-
-  function badgesOf(address _owner) constant returns (uint256 badge) {
-    return users[_owner].badges;
+    return balances[_owner];
   }
 
   function transfer(address _to, uint256 _value) returns (bool success) {
-    if (users[msg.sender].balance >= _value && _value > 0) {
-      users[msg.sender].balance -= _value;
-      users[_to].balance += _value;
+    if (balances[msg.sender] >= _value && _value > 0) {
+      balances[msg.sender] -= _value;
+      balances[_to] += _value;
       Transfer(msg.sender, _to, _value);
       success = true;
     } else {
@@ -50,21 +120,10 @@ contract Token is TokenInterface {
     return success;
   }
 
-  function sendBadge(address _to, uint256 _value) returns (bool success) {
-    if (users[msg.sender].badges >= _value && _value > 0) {
-      users[msg.sender].badges -= _value;
-      users[_to].badges += _value;
-      Transfer(msg.sender, _to, _value);
-      return true;
-    } else {
-      return false;
-    }
-  }
-
   function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-    if (users[_from].balance >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
-      users[_to].balance += _value;
-      users[_from].balance -= _value;
+    if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
+      balances[_to] += _value;
+      balances[_from] -= _value;
       allowed[_from][msg.sender] -= _value;
       Transfer(_from, _to, _value);
       return true;
@@ -85,13 +144,12 @@ contract Token is TokenInterface {
 
   function mint(address _owner, uint256 _amount) ifSales returns (bool success) {
     totalSupply += _amount;
-    users[_owner].balance += _amount;
+    balances[_owner] += _amount;
     return true;
   }
 
   function mintBadge(address _owner, uint256 _amount) ifSales returns (bool success) {
-    totalBadges += _amount;
-    users[_owner].badges += _amount;
+    if (!Badge(badgeLedger).mint(_owner, _amount)) return false;
     return true;
   }
 
