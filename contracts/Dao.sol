38c0c96345dea23eea246bc7078a0ceaad373cbc
WenJinGe <wenjinge0424@outlook.com>
2016-04-06 23:53:38 -0700

    basic proposals

--- contracts/Dao.sol
+++ contracts/Dao.sol
@@ -27,9 +27,12 @@ contract Proposal {
   address public tokenLedger;
   uint256 public minPledges;
   uint256 public minVotes;
-  PledgeData pledgedata;
-  VoteData votedata;
+  PledgeData pledgeData;
+  VoteData voteData;
   uint8 public status;
+
+  event Pledge(address indexed _pledger, uint256 indexed _amount, bool indexed _approve);
+  event Vote(address indexed _pledger, uint256 indexed _amount, bool indexed _approve);
 
   function Proposal(address _config) {
     proposer = tx.origin;
@@ -38,29 +41,61 @@ contract Proposal {
     provider = ConfigInterface(_config).getConfigAddress("provider:address");
   }
 
-  function pledge(bool _pledge, uint256 _amount) returns (bool success) {
+  function pledgeApprove() returns (bool success) {
+    return pledge(true);
+  }
+
+  function pledgeDecline() returns (bool success) {
+    return pledge(false);
+  }
+
+  function pledge(bool _pledge) returns (bool success) {
     uint256 _allowance = Badge(badgeLedger).allowance(msg.sender, address(this));
     if (_allowance <= 0) return false;
     if (!Badge(badgeLedger).transferFrom(msg.sender, address(this), _allowance)) return false;
-    pledgedata.balances[msg.sender] = _allowance;
-    if (_pledge == true) {
-      pledgedata.totalApproves++;
-    } else {
-      pledgedata.totalDeclines++;
-    }
+    if (_pledge == true) pledgeData.totalApproves += _allowance;
+    if (_pledge == false)  pledgeData.totalDeclines += _allowance;
+    pledgeData.balances[msg.sender] = _allowance;
+    Pledge(msg.sender, _allowance, _pledge);
     return true;
   }
 
+  function getInfo() public constant returns (uint256 pstartdate, uint256 penddate, uint256 papproves, uint256 pdeclines, uint256 ptotals, uint256 vstartdate, uint256 venddate, uint256 vapproves, uint256 vdeclines, uint256 vtotals) {
+    (pstartdate, penddate, papproves, pdeclines, ptotals) = (pledgeData.startDate, pledgeData.endDate, pledgeData.totalApproves, pledgeData.totalDeclines, (pledgeData.totalApproves + pledgeData.totalDeclines));
+    (vstartdate, venddate, vapproves, vdeclines, vtotals) = (voteData.startDate, voteData.endDate, voteData.totalApproves, voteData.totalDeclines, (voteData.totalApproves + voteData.totalDeclines));
+    return (pstartdate, penddate, papproves, pdeclines, ptotals, vstartdate, venddate, vapproves, vdeclines, vtotals);
+  }
+
+  function getUserInfo(address _user) public constant returns (uint256 pledgecount, uint256 votecount) {
+    return (pledgeData.balances[_user], voteData.balances[_user]);
+  }
+
+  function getMyInfo() public constant returns (uint256 pledgecount, uint256 votecount) {
+    return getUserInfo(msg.sender);
+  }
+
+  function voteApprove() returns (bool success) {
+    return vote(true);
+  }
+
+  function voteDecline() returns (bool success) {
+    return vote(false);
+  }
+
   function vote(bool _vote) returns (bool success) {
+    return true;
   }
 
   function resolve() returns (bool success) {
+    return true;
   }
 
-  function releaseBadges() {
+  function releaseBadges() returns (bool success) {
+    return true;
   }
 
-  function releaseTokens() {
+  function releaseTokens() returns (bool success) {
+    return true;
   }
 
   function budget() public constant returns (uint256 _weibudget) {
@@ -82,6 +117,8 @@ contract Dao {
   address public badgeLedger;
   mapping (uint256 => address) tokenSales;
   mapping (address => ProposalData) proposals;
+  mapping (uint256 => address) proposalIndex;
+  uint256 public proposalsCount;
 
   event NewProposal(address indexed _proposal, bytes32 indexed _document, uint256 indexed _budget);
 
@@ -90,6 +127,7 @@ contract Dao {
     owner = msg.sender;
     tokenLedger = ConfigInterface(config).getConfigAddress("ledger");
     badgeLedger = Token(tokenLedger).badgeLedger();
+    proposalsCount = 0;
   }
 
   function propose(bytes32 _document, uint256 _weibudget) {
@@ -98,7 +136,13 @@ contract Dao {
     address _proposal = new Proposal(config);
     proposals[_proposal].budget = _weibudget;
     proposals[_proposal].document = _document;
+    proposalsCount++;
+    proposalIndex[proposalsCount] = _proposal;
     NewProposal(_proposal, _document, _weibudget);
+  }
+
+  function getProposal(uint256 _index) public constant returns (address proposal) {
+    return proposalIndex[_index];
   }
 
   function funds() public constant returns (uint256 weifunds) {
