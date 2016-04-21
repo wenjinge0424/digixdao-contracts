3efaddb97a8134df22ffa61e82645fa3d3ebdf51
WenJinGe <wenjinge0424@outlook.com>
2016-04-21 08:38:07 -0700

    fix for 5.4.1

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -131,7 +131,7 @@ contract Token {
   }
 
   event Transfer(address indexed _from, address indexed _to, uint256 _value);
-  event Mint(address indexed _recipient, uint256 indexed _amount);
+  event Mint(address indexed _recipient, uint256  _amount);
   event Approval(address indexed _owner, address indexed _spender, uint256  _value);
 
   function Token(address _config) {
