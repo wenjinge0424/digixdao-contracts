4cda76bb72e01f244b372fea2dc339fa75b67c86
WenJinGe <wenjinge0424@outlook.com>
2016-05-19 10:14:37 -0700

    update genesis block init

--- unlock.js
+++ unlock.js
@@ -1,7 +1,7 @@
 var password = "DIGIXTESTER123"
 
 eth.accounts.forEach(function(account) {
-  personal.unlockAccount(account, password);
+  personal.unlockAccount(account, password, 31536000);
   console.log("Unlocked " + account);
 });
 
